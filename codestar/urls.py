5837f195626885517a2af2c14cb83ef133128e50
Leigh Allen <lallenation@gmail.com>
2024-02-03 12:53:13 +0000

    linked up json file and ammended admin page

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = [ 
     path("blog/", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
+    path('admin/', admin.site.urls),
 ]
