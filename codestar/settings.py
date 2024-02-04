35e682378ba3c2df1329688219f50f3d06fdbd7e
Leigh Allen <lallenation@gmail.com>
2024-02-04 22:32:09 +0000

    turn off debugging to deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-leighallend-newdjangobl-nyom42czuoj.ws-eu108.gitpod.io', 'la-new-django-blog-ci-4543ffed97dc.herokuapp.com']
 
